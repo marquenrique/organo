a01aa987de3dfde7d54ee7b828a43eabfecff336
Marcos H <95144034+marquenrique@users.noreply.github.com>
2022-07-30 12:44:14 -0300

    Update Formulario.js

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -31,7 +31,7 @@ export const Formulario = (props) => {
         <section className="formulario">
 
             <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card de colaborador</h2>
+                <h2>Preencha os dados para criar o card de funcionário</h2>
                 <CampoTexto valor={nome} aoAlterado={valor => setNome(valor)} obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto valor={cargo} aoAlterado={valor => setCargo(valor)} obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto valor={imagem} aoAlterado={valor => setImagem(valor)} label="Imagem" placeholder="Digite o endereço da imagem" />
